92ac87f7442cbac2d33d52a3b0278d384e6a38fa
Tzerrim <Me4z9Uirh42LteB66s>
2019-02-15 09:58:01 +0300

    SQL initialization script

--- src/main/database/initialization.sql
+++ src/main/database/initialization.sql
@@ -1,6 +1,46 @@
-INSERT IGNORE INTO telegram_bot_zathras.tabletops VALUES (NULL,'Shadowrun', 'Shadowrun 5-th edition');
-INSERT IGNORE INTO telegram_bot_zathras.tabletops VALUES (NULL,'Pathfinder', 'Pathfinder 1-st edition');
-INSERT IGNORE INTO telegram_bot_zathras.tabletops VALUES (NULL,'Earth dawn', '3-d Edition');
-INSERT IGNORE INTO telegram_bot_zathras.tabletops VALUES (NULL,'Travaller', '2-nd edition');
+-- Tabletops
+INSERT IGNORE INTO telegram_bot_zathras.tabletops VALUES (1,'Shadowrun', 'Shadowrun 5-th edition');
+INSERT IGNORE INTO telegram_bot_zathras.tabletops VALUES (2,'Pathfinder', 'Pathfinder 1-st edition');
+INSERT IGNORE INTO telegram_bot_zathras.tabletops VALUES (3,'Earth dawn', '3-d Edition');
+INSERT IGNORE INTO telegram_bot_zathras.tabletops VALUES (4,'Travaller', '2-nd edition');
 
-INSERT IGNORE INTO telegram_bot_zathras.books VALUES (NULL, '');
+-- Pathfinder books
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (1, 'Pathfinder - Core Rulebook', 'Basic book', 2 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (2, 'Pathfinder Advanced Players Guide', 'The most importaint addition', 2 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (3, 'Ultimate Magic', 'Booka about magic, spells and so on', 2 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (4, 'Ultimate Combat', 'Books about combat, fightn and so on', 2 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (5, 'Advanced Race Guide', 'Books about races', 2 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (6, 'Ultimate Equipment', 'Book about equipment', 2 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (7, 'Advanced Class Guide', 'Books about classes, hybrid classes', 2 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (8, 'Occult Adventures', 'Book about occuls, spirit classes', 2 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (9, 'Ultimate Intrigue', 'Book for faces and social', 2 );
+
+-- Shadowrun books
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (10, 'Shadowrun 5th-ed Core Rulebook', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (11, 'Aetherology', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (12, 'Bloody Business', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (13, 'Book of the Lost', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (13, 'Bullets & Bandages', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (14, 'Chrome Flesh', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (15, 'Court of Shadows', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (16, 'Cutting Aces', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (17, 'Dark Terrors', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (18, 'Data Trails', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (19, 'Forbidden Arcana', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (20, 'Gun H(e)aven', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (21, 'Hard Targets', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (22, 'Howling Shadows', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (23, 'Kill Code', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (24, 'Rigger 5.0', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (25, 'Run & Gun', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (26, 'Run Faster', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (27, 'Shadow Spells', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (28, 'Stolen Souls', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (29, 'Street Grimoire', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (30, 'The Assassins Primer', 'Basic book', 1 );
+INSERT IGNORE INTO telegram_bot_zathras.books VALUES (31, 'The Complete Trog', 'Basic book', 1 );
+
+-- Players
+INSERT IGNORE INTO telegram_bot_zathras.players VALUES (1, 'Stepan Rapinchuk', 'Danstep' );
+INSERT IGNORE INTO telegram_bot_zathras.players VALUES (2, 'Roman Timosuk', 'Ororororme' );
+INSERT IGNORE INTO telegram_bot_zathras.players VALUES (3, '', '' );

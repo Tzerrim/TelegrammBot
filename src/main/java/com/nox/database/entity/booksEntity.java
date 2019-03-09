090b0710a2d95f87703a9f325124250bb054645a
nox <shadowsirius@gmail.com>
2019-03-09 16:58:48 +0300

    Next

---
+++ src/main/java/com/nox/database/entity/booksEntity.java
@@ -0,0 +1,4 @@
+package com.nox.database.entity;
+
+public class booksEntity {
+}
---
+++ src/main/java/com/nox/database/entity/tabletopEntity.java
@@ -0,0 +1,7 @@
+package com.nox.database.entity;
+
+public class tabletopEntity {
+
+    private int id;
+    private String
+}

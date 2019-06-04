bd2ddd19c8d0749b6133a7de9d4ad9237cb9d421
nox <shadowsirius@gmail.com>
2019-06-04 10:35:09 +0300

    DAO interfaces

---
+++ src/main/java/com/nox/database/dao/interf/CharacterDAO.java
@@ -0,0 +1,19 @@
+package com.nox.database.dao.interf;
+
+import com.nox.database.entity.Tabletop;
+
+import java.util.List;
+
+
+public interface CharacterDAO {
+
+    public Character findById(int id);
+
+    public void save(Character character);
+
+    public void update(Character character);
+
+    public void delete(Character character);
+
+    public List<Character> findAllTabletops();
+}
---
+++ src/main/java/com/nox/database/dao/interf/PlayerDAO.java
@@ -0,0 +1,21 @@
+package com.nox.database.dao.interf;
+
+import com.nox.database.entity.Book;
+import com.nox.database.entity.Player;
+
+import java.util.List;
+
+
+public interface PlayerDAO {
+
+    public void save(Player player);
+
+    public void update(Player player);
+
+    public void delete(Player player);
+
+    public Book findById(int id);
+
+    public List<Book> findAllBooks();
+
+}

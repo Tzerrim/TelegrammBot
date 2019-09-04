6dc55da05888ff57071533b5cbc1c48513c0c2f8
Tzerrim <shadowsirius@gmail.com>
2019-09-04 21:08:49 +0300

    Removing trash

--- src/main/java/com/nox/main/main.java
+++ src/main/java/com/nox/main/main.java
@@ -1,9 +1,5 @@
 package com.nox.main;
 
-import com.nox.database.entity.Book;
-import com.nox.database.entity.Tabletop;
-import com.nox.database.service.BookService;
-import com.nox.database.service.TabletopService;
 import com.nox.telegram.Bot;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,23 +12,6 @@ public class main {
 
     static Logger logger =   LogManager.getLogger(main.class);
     public static void main(String[] args) {
-
-
-////        BookService bookService = new BookService();
-////        Book book = new Book();
-////        book.setId(90);
-////        book.setDescription("olololololol");
-////        book.setName("olo2l2o2lol2ol2");
-////        bookService.saveBook(book);
-//
-//
-//        TabletopService tabletopService = new TabletopService();
-//        Tabletop tabletop = new Tabletop();
-//        tabletop.setDescription("olololololol");
-//        tabletop.setName("olo2l2o2lol2ol2");
-//        logger.info(tabletop.toString());
-//        tabletopService.saveTabletop(tabletop);
-
         if ( args.length < 2 || args.length > 2) {
             logger.info("Wrong input data");
         }
@@ -53,9 +32,10 @@ public class main {
                 logger.info("Register bot");
             } catch (TelegramApiException e) {
                 logger.error(e.getMessage());
-//                dao.printStackTrace();
             }
         }
+
+
 
     }
 }
